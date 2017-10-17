adf49e973a32fb05e6bcce49c125ed56815bb089
ChengYien <cye_orange@163.com>
2017-10-17 13:21:35 +0800

    new

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* 
+* @Author: Marte
+* @Date:   2017-05-26 11:03:33
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-08-28 22:57:00
+*/
+
+$(document).ready(function(){
+    var mySwiper = new Swiper ('#bannerSwiper', {
+        loop: true,
+        autoplay: 2000,
+        pagination: '.swiper-pagination'
+    })        
+});
